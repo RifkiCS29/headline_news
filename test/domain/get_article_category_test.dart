16e16ab434c85a56c3494e2ae31d55aa2d8eb566
Rifki <rifkipelajar@gmail.com>
2022-01-02 23:22:32 +0800

    Get Article Category Test

---
+++ test/domain/get_article_category_test.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:headline_news/domain/entities/article.dart';
+import 'package:headline_news/domain/usecases/get_article_category.dart';
+import 'package:mockito/mockito.dart';
+
+import '../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetArticleCategory usecase;
+  late MockArticleRepository mockArticleRepository;
+
+  setUp(() {
+    mockArticleRepository = MockArticleRepository();
+    usecase = GetArticleCategory(mockArticleRepository);
+  });
+
+  final tCategory = 'business';
+  final tArticles = <Article>[];
+
+  test('should get list of Article Category from the repository',
+      () async {
+    // arrange
+    when(mockArticleRepository.getArticleCategory(tCategory))
+        .thenAnswer((_) async => Right(tArticles));
+    // act
+    final result = await usecase.execute(tCategory);
+    // assert
+    expect(result, Right(tArticles));
+  });
+}
