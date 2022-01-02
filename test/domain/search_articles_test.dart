e5750717f678b23fb2072b84f3cb0592f436f6a2
Rifki <rifkipelajar@gmail.com>
2022-01-02 23:28:40 +0800

    Get Search Article Test

---
+++ test/domain/search_articles_test.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:headline_news/domain/entities/article.dart';
+import 'package:headline_news/domain/usecases/search_articles.dart';
+import 'package:mockito/mockito.dart';
+
+import '../helpers/test_helper.mocks.dart';
+
+void main() {
+  late SearchArticles usecase;
+  late MockArticleRepository mockArticleRepository;
+
+  setUp(() {
+    mockArticleRepository = MockArticleRepository();
+    usecase = SearchArticles(mockArticleRepository);
+  });
+
+  final tArticles = <Article>[];
+  final tQuery = 'business';
+
+  test('should get list of Articles from the repository', () async {
+    // arrange
+    when(mockArticleRepository.searchArticles(tQuery))
+        .thenAnswer((_) async => Right(tArticles));
+    // act
+    final result = await usecase.execute(tQuery);
+    // assert
+    expect(result, Right(tArticles));
+  });
+}
