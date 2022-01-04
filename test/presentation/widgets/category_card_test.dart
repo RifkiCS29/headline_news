debae149d22c3180fb42749cdabd2df9d7565e51
Rifki <rifkipelajar@gmail.com>
2022-01-04 20:30:40 +0800

    Category Card Test

---
+++ test/presentation/widgets/category_card_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:headline_news/presentation/widgets/widgets.dart';
+
+void main() {
+  final String name = 'name';
+  final String image = 'assets/science.png';
+
+  group('Top Headline Article card Widget Test', () {
+    Widget _makeTestableWidget() {
+      return MaterialApp(home: Scaffold(body: CategoryCard(name, image)));
+    }
+
+    testWidgets('Testing if title Article shows', (WidgetTester tester) async {
+      await tester.pumpWidget(_makeTestableWidget());
+      expect(find.byType(Text), findsOneWidget);
+      expect(find.byType(Container), findsWidgets);
+    });
+  });
+}
