ff00d89388e65471b491ffab9c7bdb6dff0f2d98
RifkiCS29 <rifkipelajar@gmail.com>
2025-12-12 21:59:26 +0700

    update getx

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -1,4 +1,4 @@
 const String baseUrl = 'https://newsapi.org/v2/';
-const String apiKey = 'your_api_key';
-const String country = 'id';
+const String apiKey = '48f6fed0961645209da05da0c15088aa';
+const String country = 'us';
 const int pageSize = 20;
